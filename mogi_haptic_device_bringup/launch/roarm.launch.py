e22f97e50cc38303d78303258ab0bfba42392aff
David Dudas <david.dudas@outlook.com>
2025-10-07 22:45:38 +0200

    add launchfiles for bringup

---
+++ mogi_haptic_device_bringup/launch/roarm.launch.py
@@ -0,0 +1,117 @@
+# Author: Williams
+# Date: Nov 1, 2022
+# Description: Launch a RoArm-M1 URDF file using Rviz.
+# https://waveshare.com
+ 
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+ 
+def generate_launch_description():
+ 
+  # Set the path to this package.
+  roarm_pkg_share = FindPackageShare(package='roarm').find('roarm')
+  mogi_pkg_share = FindPackageShare(package='mogi_haptic_device_bringup').find('mogi_haptic_device_bringup')
+ 
+  # Set the path to the RViz configuration settings
+  default_rviz_config_path = os.path.join(mogi_pkg_share, 'rviz/rviz.rviz')
+ 
+  # Set the path to the URDF file
+  default_urdf_model_path = os.path.join(roarm_pkg_share, 'urdf/roarm.urdf')
+ 
+  ########### YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE ##############  
+  # Launch configuration variables specific to simulation
+  jsp = LaunchConfiguration('jsp')
+  urdf_model = LaunchConfiguration('urdf_model')
+  rviz_config_file = LaunchConfiguration('rviz_config_file')
+  use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+  use_rviz = LaunchConfiguration('use_rviz')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+ 
+  # Declare the launch arguments  
+  declare_urdf_model_path_cmd = DeclareLaunchArgument(
+    name='urdf_model', 
+    default_value=default_urdf_model_path, 
+    description='Absolute path to robot urdf file')
+     
+  declare_rviz_config_file_cmd = DeclareLaunchArgument(
+    name='rviz_config_file',
+    default_value=default_rviz_config_path,
+    description='Full path to the RVIZ config file to use')
+     
+  declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
+    name='jsp',
+    default_value='False',
+    description='Flag to enable joint_state_publisher_gui')
+   
+  declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+    name='use_robot_state_pub',
+    default_value='True',
+    description='Whether to start the robot state publisher')
+ 
+  declare_use_rviz_cmd = DeclareLaunchArgument(
+    name='use_rviz',
+    default_value='True',
+    description='Whether to start RVIZ')
+     
+  declare_use_sim_time_cmd = DeclareLaunchArgument(
+    name='use_sim_time',
+    default_value='False',
+    description='Use simulation (Gazebo) clock if true')
+    
+  # Specify the actions
+ 
+  # A GUI to manipulate the joint state values
+  start_joint_state_publisher_gui_node = Node(
+    condition=IfCondition(jsp),
+    package='joint_state_publisher_gui',
+    executable='joint_state_publisher_gui',
+    name='joint_state_publisher_gui')
+ 
+  # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+  start_robot_state_publisher_cmd = Node(
+    condition=IfCondition(use_robot_state_pub),
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': use_sim_time, 
+    'robot_description': Command(['xacro ', urdf_model])}],
+    arguments=[default_urdf_model_path])
+ 
+  # Launch RViz
+  start_rviz_cmd = Node(
+    condition=IfCondition(use_rviz),
+    package='rviz2',
+    executable='rviz2',
+    name='rviz2',
+    output='screen',
+    arguments=['-d', rviz_config_file])
+
+  start_serial_ctrl = Node(
+    package='serial_ctrl',
+    executable='serial_ctrl',
+    name='serial_ctrl',
+    output='screen',
+    parameters=[{'use_sim_time': use_sim_time}])
+   
+  # Create the launch description and populate
+  ld = LaunchDescription()
+ 
+  # Declare the launch options
+  ld.add_action(declare_urdf_model_path_cmd)
+  ld.add_action(declare_rviz_config_file_cmd)
+  ld.add_action(declare_use_joint_state_publisher_cmd)
+  ld.add_action(declare_use_robot_state_pub_cmd)  
+  ld.add_action(declare_use_rviz_cmd) 
+  ld.add_action(declare_use_sim_time_cmd)
+ 
+  # Add any actions
+  ld.add_action(start_joint_state_publisher_gui_node)
+  ld.add_action(start_robot_state_publisher_cmd)
+  ld.add_action(start_rviz_cmd)
+  ld.add_action(start_serial_ctrl)
+ 
+  return ld
---
+++ mogi_haptic_device_bringup/launch/telemanipulator.launch.py
@@ -0,0 +1,32 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+  use_sim_time = LaunchConfiguration('use_sim_time')
+
+  declare_use_sim_time_cmd = DeclareLaunchArgument(
+    name='use_sim_time',
+    default_value='False',
+    description='Use simulation (Gazebo) clock if true')
+
+  start_telemanipulator_driver = Node(
+    package='mogi_haptic_device_py',
+    executable='serial_driver',
+    name='telemanipulator_driver',
+    output='screen',
+    parameters=[{'use_sim_time': use_sim_time}])
+   
+  # Create the launch description and populate
+  ld = LaunchDescription()
+ 
+  # Add any actions
+  ld.add_action(declare_use_sim_time_cmd)
+  ld.add_action(start_telemanipulator_driver)
+ 
+  return ld
